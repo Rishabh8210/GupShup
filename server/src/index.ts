e40b989c9fa24b0de81a004a7d0197994d81d80a
Rishabh Pandey <rishabhpandey8092@gmail.com>
2025-06-17 00:10:22 +0530

    feat: Added global error handler

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response, urlencoded} from 'express'
 import { PORT } from './config/server-config';
 import ApiRoutes from './routes'
 import { setupDatabase } from './config/db-config';
+import { GlobalErrorHandler } from './middleware/global-error-handler';
 
 const app = express()
 app.use(express.json())
@@ -11,6 +12,15 @@ app.use(express.urlencoded({extended: true}));
 setupDatabase();
 
 app.use('/api', ApiRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error(`Route ${req.originalUrl} not found`);
+    // error.statusCode = 404;
+    next(error);
+});
+
+// global error handler
+app.use(GlobalErrorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server is Running at PORT:${PORT}`);
